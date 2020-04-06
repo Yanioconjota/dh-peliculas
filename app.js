2b7b46c0ad55dc4f5273cc265be18cf9d9d0bf89
Janio Isacura <jisacura@baufest.com>
2020-04-06 18:51:08 -0300

    Se actualizó módulo de ruteo

--- app.js
+++ app.js
@@ -2,48 +2,8 @@ const http = require('http'),
       // open = require('open'),
       hostname = 'localhost',
       port = '3030',
-      home = require('./src/homePage'),
-      enCartelera = require('./src/enCartelera'),
-      masVotadas = require('./src/masVotadas'),
-      contacto = require('./src/contacto'),
-      faq = require('./src/preguntasFrecuentes'),
-      sucursales = require('./src/sucursales');
+      rutas = require('./router');
 
-http.createServer((req, res) => {
-  // Route System
-   res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-  switch (req.url) {
-    case '/':
-    case 'home':
-      res.end(home);
-      break;
-
-    case '/en-cartelera':
-      res.end(enCartelera);
-      break;
-    
-    case '/mas-votadas':
-      res.end(masVotadas);
-      break;
-    
-    case '/sucursales':
-      res.end(sucursales);
-      break;
-
-    case '/contacto':
-      res.end(contacto);
-      break;
-    
-      case '/preguntas-frecuentes':
-      case '/faq':
-      res.end(faq);
-      break;
-
-    default:
-      res.end('Server running in ' + port + ' port')
-      break;
-  }
-}).listen(port,  hostname, () => {
+rutas.listen(port,  hostname, () => {
   console.log('Server running in ' + port + ' port');
-  //open('http://' + hostname + ':' + port);
 });
